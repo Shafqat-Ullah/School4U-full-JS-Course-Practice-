a2d3b582974155f4316179aa07453fa9361ab05e
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-01-26 01:48:07 +0500

    Practical Example

--- 02_Variable And Datatypes/script.js
+++ 02_Variable And Datatypes/script.js
@@ -99,6 +99,43 @@ console.log(name2 , age , isStudent , marks , fee);
 
 
 
+// Use Case 1:
+// Suppose price of two products comes from database, you need to store these prices and show the total as output.
+
+let price1 = 30;
+let price2  = 50;
+let sum = price1 + price2 ;
+console.log(sum);
+
+
+// Use Case 2:
+// A user enters their first and last name into a form, and you need to display the full name.
+
+let firstName = "Shafqat ";
+let lastName = "Ullah";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+
+
+// Q1: How do you declare a variable in JavaScript?
+// Q2: What is the difference between var, let, and const?
+// Q3: Can you change the value of a const variable?
+// Q4: What will happen if you use a variable without declaring it?
+// Q5: What is the default value of an uninitialized variable in JavaScript?
+// Q6: What are the primitive data types in JavaScript?
+// Q7: What is the difference between null and undefined?
+// Q8: Is JavaScript a statically typed or dynamically typed language?
+// Q9: What will be the output of typeof null?
+// Q10: What happens when you add a number and a string in JavaScript?
+// Q11: What is type coercion? Give an example.
+// Q12: How can you manually convert a string to a number in JavaScript?
+// Q13: What is the result of "5" - 3 in JavaScript?
+// Q14: What is NaN in JavaScript, and how do you check if a value is NaN?
+// Q15: How do you check the type of a variable in JavaScript?
+
+
 
 
 
