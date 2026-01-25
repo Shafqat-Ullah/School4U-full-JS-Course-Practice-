9e44f3fe7db412e673f91cb46a64853626aacabb
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-01-26 00:55:50 +0500

    variables

--- 02_Variable And Datatypes/script.js
+++ 02_Variable And Datatypes/script.js
@@ -16,8 +16,33 @@
 
 
 // ................. variables ..............
-const abc = 20;
-console.log(abc$);
+// const abc = 20;
+// console.log(abc$);
+
+
+
+// 🔹 Easy Practice Questions 📝
+// 1️⃣ Variable bana kar apna name store karo
+// 2️⃣ Age variable bana kar update karo
+// 3️⃣ const mein country store karo
+
+let name = "Shafqat Ullah";
+console.log(name);
+ 
+let num = 12;
+num = 15;
+console.log(num);
+ 
+
+let age = 20;
+age = 25;
+console.log(age);
+
+const country = "Pakistan";
+console.log(country);
+
+
+
 
 
   
