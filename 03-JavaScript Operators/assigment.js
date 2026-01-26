6797b3a43d383b9c2cf578a77bd33b0889eebdee
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-01-27 02:14:28 +0500

    Assignment Operators

--- 03-JavaScript Operators/assigment.js
+++ 03-JavaScript Operators/assigment.js
@@ -3,6 +3,21 @@ console.log(a);
 a = a+5;
 console.log(a);
 a = a +6;
+console.log(a);
+
+
+a+= 7;  //(a+=7 are = to like a= a+7)
+console.log(a);
+
+a-= 10;
+console.log("the - of a is :",a);
+
+
+// 	Example
+// =	x = 5
+// +=	x += 3
+// -=	x -= 2
+// *=	x *= 2
 
 
 
