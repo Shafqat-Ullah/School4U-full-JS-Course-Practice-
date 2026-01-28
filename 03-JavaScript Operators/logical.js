6026168bea73b736b497f1570e44f43aca5a35ac
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-01-28 23:38:58 +0500

    logical

--- 03-JavaScript Operators/logical.js
+++ 03-JavaScript Operators/logical.js
@@ -16,16 +16,24 @@
 // console.log("!false:",!false);
 
 // ...................................................
-let a = 20;
-console.log("AND OPERATOR: a>=10 && a<= 100",a>=10 && a<= 100);
-console.log("AND OPERATOR: a>=10 && a<= 100",a>=190 && a<= 100);
-// ...........................................................
-let b = 50;
-console.log("OR operatore : b>=100 || b<=100", b>=100 || b<=100);
-console.log("OR operatore : b>=100 || b<=100", b>=100 || b<=40);
+// let a = 20;
+// console.log("AND OPERATOR: a>=10 && a<= 100",a>=10 && a<= 100);
+// console.log("AND OPERATOR: a>=10 && a<= 100",a>=190 && a<= 100);
+// // ...........................................................
+// let b = 50;
+// console.log("OR operatore : b>=100 || b<=100", b>=100 || b<=100);
+// console.log("OR operatore : b>=100 || b<=100", b>=100 || b<=40);
 
 
 
+// and 
+// true true = true 
+// true false = false 
+// false true = false;
+// false false = false 
+
+// NULLISH COALESCING 
+// returns the right haNd value only if the left is null or undefined.
 
 
 
