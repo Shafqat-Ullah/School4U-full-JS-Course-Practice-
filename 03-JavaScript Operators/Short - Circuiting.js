7bc2301fac05aca46ef46fcce5b7aae2f03abc3c
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-01-29 01:04:41 +0500

    short

--- 03-JavaScript Operators/Short - Circuiting.js
+++ 03-JavaScript Operators/Short - Circuiting.js
@@ -1,28 +1,61 @@
 
-                            // Short - Circuiting
-//  OR ma or ko jab be tryth value mel jie to os ka output ture ho chiea vo 1st value ho 2nd ho ye last
-let result = (""||"shafqat"||null)
-console.log(result); //output is shafqat beacuse the shafqat is truthy value ;
+//                             // Short - Circuiting
+// //  OR ma or ko jab be tryth value mel jie to os ka output ture ho chiea vo 1st value ho 2nd ho ye last
+// let result = (""||"shafqat"||null)
+// console.log(result); //output is shafqat beacuse the shafqat is truthy value ;
  
 
-let or = ("khan"||"ahmad"||null);
-console.log(or);//the output is khan beacuse khan is the frist truthy value
+// let or = ("khan"||"ahmad"||null);
+// console.log(or);//the output is khan beacuse khan is the frist truthy value
 
-// AND  AND ma agar and ko false ghia be  ho to val serip false show karige other wasie LAST MA  jo be truthy value ho to vo show kariga
-let AND = ("KHAN"&&"AHMAD"&&undefined);
-console.log(AND);//OUTPUT IS UNDFINE
+// // AND  AND ma agar and ko false ghia be  ho to val serip false show karige other wasie LAST MA  jo be truthy value ho to vo show kariga
+// let AND = ("KHAN"&&"AHMAD"&&undefined);
+// console.log(AND);//OUTPUT IS UNDFINE
 
-let AND2 = ("SHAFQAT"&&"ULLAH"&&"KHAN");
-console.log(AND2);
+// let AND2 = ("SHAFQAT"&&"ULLAH"&&"KHAN");
+// console.log(AND2);
 
 
-// 3. ?? (Nullish Coalescing): Returns the right-hand value only if the left is null or undefined.
-//  (Better than || when dealing with falsy values like 0 or "" that are still valid.)
+// // 3. ?? (Nullish Coalescing): Returns the right-hand value only if the left is null or undefined.
+// //  (Better than || when dealing with falsy values like 0 or "" that are still valid.)
 
-let Nullish = (null ?? "ABBBB");
-console.log(Nullish);
-let Nullish2 = ("ABCDSC"?? null);
-console.log(Nullish2);
+
+
+
+// let Nullish = (null ?? "ABBBB");
+// console.log(Nullish);
+// let Nullish2 = ("ABCDSC"?? null);
+// console.log(Nullish2);
+
+
+// let a = (12 ?? null)
+// console.log(a);
+ 
+// let b = (null ?? 12)
+// console.log(b);
+
+let name = null;
+let result = name ?? "Guest";
+
+console.log(result);
+
+let name2 = 12;
+let result2 = name2 ?? "";
+
+console.log(result2);
+
+
+
+
+
+
+// let a= (""||""||"khan");
+// console.log(a);
+
+// let khan = ('g'&&12&&"")
+// console.log(khan);
+// let not = (!12);
+// console.log(not);
 
 
 
