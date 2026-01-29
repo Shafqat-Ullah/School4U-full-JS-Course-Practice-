1471744f021951a87c93d148c0db9409ddca97ff
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-01-29 15:58:47 +0500

    ternary

--- 03-JavaScript Operators/ternary.js
+++ 03-JavaScript Operators/ternary.js
@@ -9,12 +9,18 @@
 // console.log(result);
 
 
-let age = 20;
-let result = age>=20 ? "you can drive :":"can not drive"
+// let age = 20;
+// let result = age>=20 ? "you can drive :":"can not drive"
+// console.log(result);
+
+
+// let username = " ";
+// let passowerd = " ";
+// let message = (username&&passowerd)? "login succesfully":"tryagain"
+// console.log(message);
+
+
+// 3. Even / Odd using ternary operator
+let num = +prompt("Enter a number ")
+let result = (num % 2 ===0) ? "Even Number":"odd number";
 console.log(result);
-
-
-let username = " ";
-let passowerd = " ";
-let message = (username&&passowerd)? "login succesfully":"tryagain"
-console.log(message);
