f7a89149f5da61004f505c3c560391d644280189
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-01-29 11:52:31 +0500

    arthmetic

--- 03-JavaScript Operators/arithmetic.js
+++ 03-JavaScript Operators/arithmetic.js
@@ -18,21 +18,28 @@ console.log(a % b); // 1
 // On a shopping website, calculate the total cost of a product when given the price per item (price = 150) and the quantity (quantity = 3). 
 // Also, calculate a 10% discount on the total cost and display the discounted price.
 
-let item = 150;
-let quality = 3;
-let totalCost = item *quality;
-let discount = totalCost *0.10;
-let price = totalCost-discount;
-console.log("The item price is :", item);
-console.log("The qulity of item is :", quality);
-console.log("The total coust is ",totalCost);
-console.log("The discount is ",discount);
-console.log("the total price of the iteam is:" , price);
+// let item = 150;
+// let quality = 3;
+// let totalCost = item *quality;
+// let discount = totalCost *0.10;
+// let price = totalCost-discount;
+// console.log("The item price is :", item);
+// console.log("The qulity of item is :", quality);
+// console.log("The total coust is ",totalCost);
+// console.log("The discount is ",discount);
+// console.log("the total price of the iteam is:" , price);
+
 
 
 
   
-
+// 1. Two numbers input + all arithmetic results
+let num1 = +prompt("Enter the value of num1 ")
+let num2 =+prompt("Enter the vlaue of num2 ");
+console.log("the Sum of num and num2", num1 + num2);
+console.log("the - of num and num2", num1 - num2);
+console.log("the / of num and num2", num1 / num2);
+console.log("the * of num and num2", num1 * num2);
 
 
 
