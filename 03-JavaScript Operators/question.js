bcc4cac2bff66c8cf0da7762a5b9a5964f65bae6
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-01-30 12:07:18 +0500

    JS Question

--- 03-JavaScript Operators/question.js
+++ 03-JavaScript Operators/question.js
@@ -1,11 +1,25 @@
 // 1. Get user to input two number using prompt and print their possible arithmetic results.
-let num1 = Number( prompt("Enter The Value Of Num1"));
-let num2 = Number(prompt("Enter the value of num2"));
-console.log(num1+num2);
-console.log(num1-num2);
-console.log(num1/num2)
-console.log(num1*num2)
-console.log(num1%num2)
+// let num1 = Number( prompt("Enter The Value Of Num1"));
+// let num2 = Number(prompt("Enter the value of num2"));
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1/num2)
+// console.log(num1*num2)
+// console.log(num1%num2)
 
 // 2. Can you chain assignment operators?
-let a=b=c=d =10;
+// let a=b=c=d =10; 
+
+// 7. Email & Password check
+
+let StoreEmail = "Shafqat@gmail.com";
+let StorePassword = "Shafqat321";
+let Email = prompt("Enter Your Email");
+let Password = prompt("Enter Your Passoword");
+if (StoreEmail===Email && StorePassword===Password){
+    console.log("Login Successfully");
+    
+}else{
+    console.log("Invalid Email or Password");
+    
+}
