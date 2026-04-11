a9065f3f9a7ff8f0790dc0f21c0afff68a104fbe
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-04-11 15:14:26 +0500

    object

--- 08-Objects vs String vs Arrays in JavaScript/object.js
+++ 08-Objects vs String vs Arrays in JavaScript/object.js
@@ -1,22 +1,35 @@
 // .....................Object .......................
-let car ={
-    brand : "honda",
-    color : "red",
-    maxSpeed : 200,
-    stop : ()=>{
-        console.log("stop the car");
-        return 1;
+// let car ={
+//     brand : "honda",
+//     color : "red",
+//     maxSpeed : 200,
+//     stop : ()=>{
+//         console.log("stop the car");
+//         return 1;
 
-    },
-    start : function(){
-        console.log("Starting the Car");
-        return "Start Successfull"
-    }
-}
+//     },
+//     start : function(){
+//         console.log("Starting the Car");
+//         return "Start Successfull"
+//     }
+// }
 // console.log(car);
 // console.log(car.brand);
 // car.start();
 // car.stop()
-console.log(car["brand"]);
+// console.log(car["brand"]);
 
 // car ["start"]
+
+
+let obj = new Object({
+    name : "Shafqat Ullah"
+})
+let obj2 = {
+    number : 123,
+    city: "pakistan"
+}
+obj.age = 19
+console.log(obj);
+console.log(obj2);
+
