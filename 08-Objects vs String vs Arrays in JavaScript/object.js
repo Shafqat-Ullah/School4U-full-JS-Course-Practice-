eef7d56ba88d6caf111d2ca34daa0bada84bdff7
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-04-11 18:17:23 +0500

    object

--- 08-Objects vs String vs Arrays in JavaScript/object.js
+++ 08-Objects vs String vs Arrays in JavaScript/object.js
@@ -22,14 +22,75 @@
 // car ["start"]
 
 
-let obj = new Object({
-    name : "Shafqat Ullah"
-})
-let obj2 = {
-    number : 123,
-    city: "pakistan"
-}
-obj.age = 19
-console.log(obj);
-console.log(obj2);
+// let obj = new Object({
+//     name : "Shafqat Ullah"
+// })
+// let obj2 = {
+//     number : 123,
+//     city: "pakistan"
+// }
+// obj.age = 19
+// console.log(obj);
+// console.log(obj2);
 
+// let obj = {
+//     name : "Shafqat Ullah",
+//     age : 23
+
+// }
+// console.log(obj);
+
+// let obj = {
+//     name :"shafqat",
+//     greet : function(){
+//         console.log(`hello this ${this.name}`)
+        
+//     } ,
+// }
+// obj.greet()
+
+// let obj = {
+// name: "Manas Kumar Lal",
+// greet : function(){
+// console.log(this.name)
+// },
+// }
+// obj.greet() // Manas Kumar Lal
+
+
+// let intro = {
+//     name : "shafqat ullah",
+//     age : 20,
+//     fun : function (){
+// console.log(`the into of ${this.name}`);
+
+//     },
+// }
+//     intro.fun()
+
+
+// let obj = {
+//     name : "Shafqat Ullah",
+//     age  : 30,
+
+// }
+// let keys = Object.keys(obj)
+// console.log(keys)
+// let value = Object.values(obj)
+// console.log(value)
+// let obj ={
+//     name :"Shafqat Ullah",
+//     age : 30
+// }
+// let name = obj.name;
+// let age = obj.age;
+// console.log(name,age);
+
+
+// let obj ={
+//     name :"Ahmad",
+//     age : 12,
+//     address : "kpk"
+// }
+// let {name ,age,address}=obj;
+// console.log(name,age,address);
