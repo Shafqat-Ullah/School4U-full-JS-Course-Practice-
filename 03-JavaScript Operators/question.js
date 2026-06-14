4c45f75207b2f8abda6b6c8bac2638a792d30bff
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-14 20:04:15 +0500

    update

--- 03-JavaScript Operators/question.js
+++ 03-JavaScript Operators/question.js
@@ -8,7 +8,7 @@
 // console.log(num1%num2)
 
 // 2. Can you chain assignment operators?
-// let a=b=c=d =10; 
+// let a=b=c=d =10;
 
 // 7. Email & Password check
 
@@ -18,10 +18,10 @@
 // let Password = prompt("Enter Your Passoword");
 // if (StoreEmail===Email && StorePassword===Password){
 //     console.log("Login Successfully");
-    
+
 // }else{
 //     console.log("Invalid Email or Password");
-    
+
 // }
 
 // 8. What will be the output of the following JavaScript code?
@@ -88,8 +88,6 @@
 // console.log("result:", result); // 4
 // console.log("Type of z:", typeof z); // string
 
-
-
 // let iteam_price = 150;
 // let products = 3;
 // let total_coust = iteam_price * products;
@@ -98,33 +96,26 @@
 // console.log(`The discount is : ${ discount}`);
 // console.log(`The final price ${total_coust}`);
 
-let iteam_price = 150;
-let products = 3;
-let total_coust = iteam_price * products;
-console.log(`the total coust is ${total_coust}`);
+// let iteam_price = 150;
+// let products = 3;
+// let total_coust = iteam_price * products;
+// console.log(`the total coust is ${total_coust}`);
 
-let  discount= total_coust * 0.10;
-console.log(`The discount price is : ${discount}`);
+// let  discount= total_coust * 0.10;
+// console.log(`The discount price is : ${discount}`);
 
-let  final_price  =  total_coust - discount;
-console.log(`the final  price is ${final_price}`);
-
-
+// let  final_price  =  total_coust - discount;
+// console.log(`the final  price is ${final_price}`);
 
 // 6: Ternary Operator (Shortcut for if/else)
 
 //     let age = 30;
 //    let result = (age > 20 ) ? "adult " : "kids";
 //     console.log(result);
-    
 
 // let age = 20;
 // let result = (age > 15)? "you can drive" : "you are kids ";
 // console.log(result);
-
-
-
-
 
 // On a booking website, check if the user's age is valid for booking:
 // Age should be at least 18.L
@@ -133,15 +124,117 @@ console.log(`the final  price is ${final_price}`);
 // let result = (age > 18 )? "Eligible for booking": "Not eligible for booking ";
 // console.log(result);
 
-
 // On a login page, verify the user's credentials: Check if username is not empty AND password is not empty (&& operator).
 // If either is empty, display an error message: "Both fields are required."
 
-let user_name = "Shafqat Ullah";
-let password = 12323;
-let result = (user_name && password !== null)? "Login Successfully":"Both Fields are required" ;
-console.log(result);
+// let user_name = "Shafqat Ullah";
+// let password = 12323;
+// let result = (user_name && password !== null)? "Login Successfully":"Both Fields are required" ;
+// console.log(result);
+
+// Short - Circuiting
+// ............... OR ...............
+// let a = "" || undefined|| 12;
+// console.log(a);
+
+// // ...................AND .................
+// let result = 12 && 11 && 13 && 5555;
+// console.log(result);
+
+// let isAuthenticated = true;
+// let user = "Shafqat Ullah";
+// let result = isAuthenticated && user;
+// console.log(`${result}`);
+
+// let a = null ?? 12;
+// console.log(a);
+
+// 1. Get user to input two number using prompt and print their possible arithmetic results.
+// let num1 = +prompt("Enter frist number");
+// let num2 = +prompt("Enter second Number :");
+// console.log(
+//   `num1 + num2 is ${num1 + num2} \ num1 *  num2 ${num1 * num2} \ num1 /num2 ${num1 / num2} \ num1 - num2 ${num1 - num2}`,
+// );
 
 
+// Can you chain assignment operators
+// let a =b=c=d =15;
+// console.log(a,b,c);
 
+//   3. Get user to input a number using prompt and check whether even or odd using ternary operator.
+
+// let input = +(prompt("Enter The Value if You want to Check the number is even or odd"))
+// let result =(input % 2 === 0 )? "even ":"odd";
+// console.log(result);
+
+// 4. What is the final value of x?
+
+// let x = 5;
+// console.log(x);
+// x += 3;
+// console.log(x);
+
+// x *= 4;
+// console.log(x);
+// x /= 6;
+// console.log(x);
+// x -= 2;
+// console.log(x);
+// x %= 3;
+// console.log(x);
+
+
+// 5. Check if a number is within a range between 10 and 20 (inclusive).
+
+// let num 9= 19;
+// let result = (num >10 &&num <20)? "Range": "inclusive";
+// console.log(result);
+
+// let a=20, b= 40, c= 12;
+// let result = (a>b)?( a>c ? a:c):(b>c ? b:c)
+// console.log(result);
+
+// let email = "@shafqat";
+// let password = 1213;
+// let database_email = "@shafqat";
+// let database_password = 1213;
+// let result = ( database_email === email && database_password=== password)? "login successfully":"Email or password is incorrect";
+// console.log(result);
+
+// 8. What will be the output of the following JavaScript code?
+
+// let a = 5, b = 3, c = 2;
+
+// let result = a++ + -- b * c -- - ++a + b -- / -- c;   (WOrking on BODMs Rules)
+
+// console. log("a:", a); //3 
+// console.log("b:", b);//0
+// console.log("c:", c);
+// console.log("result:", result);
+
+// What is the output of console.log(~a) where a = 0
+// console.log(~a);
+
+
+// 9. What will be the output of the following JavaScript code?
+
+let x = 10;
+let y = 5;
+let z = "10";
+
+x += y * 2; // Arithmetic + Assignment
+let isEqual = x == z; // Loose comparison
+let isStrictEqual = x === z; // Strict comparison
+let logicTest = (isEqual || isStrictEqual) && !(y > 10); // Logical + Comparison + NOT
+
+let result = logicTest ? ++x : -- y; // Ternary + Pre-increment/Pre-decrement
+
+console.log("x:",x);
+console.log("y:", y);
+console.log("z:", z);
+console.log("isEqual:", isEqual);
+console.log("isStrictEqual:", isStrictEqual);
+console.log("logicTest:", logicTest);
+console.log("result:", result);
+console.log("Type of z:", typeof z); // Unary operator typeof
 
