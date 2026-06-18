e951e5401b5d6a68fdad0369d5d5ecdb1dd8d0fb
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-18 13:11:20 +0500

    update

--- 04-javascript conditionals/question.js
+++ 04-javascript conditionals/question.js
@@ -1,23 +1,27 @@
 // 
 // 
 
-let theme = "light";
-let color = prompt("Enter the color name that you want to select them for your side ,(white,black,green)");
-if (theme===`light`){
-    color = 'white'
-    console.log(color);
+// let theme = "light";
+// let color = prompt("Enter the color name that you want to select them for your side ,(white,black,green)");
+// if (theme===`light`){
+//     color = 'white'
+//     console.log(color);
     
-}
-else if(theme === 'dark' ){
-    color = 'black'
-    console.log(color);
+// }
+// else if(theme === 'dark' ){
+//     color = 'black'
+//     console.log(color);
     
-}
-else if (theme === 'green' ){
-    color='green';
-    console.log(color);
+// }
+// else if (theme === 'green' ){
+//     color='green';
+//     console.log(color);
     
-}else{
-    console.log("invalid Color try Again")
+// }else{
+//     console.log("invalid Color try Again")
     
-}
+// }
+
+let num1 = 12;
+let num2 = 50;
+console.log(`The * of num1 and num2 ${num1*num2}\ the + of num1 and num2 ${num1 +num2} \ the - of num1 and num2 is ${num1-num2}\the dev / of num1 and num2 is ${num1/num2}`);
