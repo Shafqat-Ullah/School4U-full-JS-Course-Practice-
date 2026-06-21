0d99a0e7e4fd46263e56d15756b2935b88407506
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-21 12:00:21 +0500

    update

--- 04-javascript conditionals/question.js
+++ 04-javascript conditionals/question.js
@@ -22,6 +22,32 @@
     
 // }
 
-let num1 = +prompt("Enter The num1 ");
-let num2 = +prompt("enter the num2 ");
-console.log(`The * of num1 and num2 ${num1*num2}\ the + of num1 and num2 ${num1 +num2} \ the - of num1 and num2 is ${num1-num2}\the dev / of num1 and num2 is ${num1/num2}`);
+// let num1 = +prompt("Enter The num1 ");
+// let num2 = +prompt("enter the num2 ");
+// console.log(`The * of num1 and num2 ${num1*num2}\ the + of num1 and num2 ${num1 +num2} \ the - of num1 and num2 is ${num1-num2}\the dev / of num1 and num2 is ${num1/num2}`);
+
+
+// - Find the smallest of three numbers. Numbers are given by the user
+
+let theme = "light";
+let color = prompt("Enter the name of the color for your theme (White,black , green)")
+if (theme === "light"){
+    color =`white`
+}else if (theme=== 'dark'){
+    color = "black"
+}else if (theme === 'green'){
+    color = 'green'
+}else{
+
+    console.log('invalid color');
+    
+}
+if (theme!=='light' &&theme!=='dark'&&theme!=="green"){
+    console.log('invalid color selection');
+    
+}
+   else 
+{
+        console.log(`your select them is ${theme} and color is :${color}`);
+    
+}
