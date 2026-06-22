9356fefe7a53ea260de33cc4633aed804012105d
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-22 10:17:24 +0500

    update

--- 05_Loops In JavaScript/script.js
+++ 05_Loops In JavaScript/script.js
@@ -572,7 +572,14 @@
     
 // }
 
-for (let i =0;i<10;i++){
+// for (let i =0;i<10;i++){
+//     console.log("Shafqat Ullah");
+    
+// }
+
+let i =0;
+while(i<10){
     console.log("Shafqat Ullah");
+    i++;
     
 }
