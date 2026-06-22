612e357f1f0ab0346d881a7bd15a4dd46ea699e7
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-22 22:59:10 +0500

    update

--- 05_Loops In JavaScript/script.js
+++ 05_Loops In JavaScript/script.js
@@ -577,9 +577,19 @@
     
 // }
 
-let i =0;
-while(i<10){
-    console.log("Shafqat Ullah");
-    i++;
+
+// while loop 
+// let i =1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
     
+// }
+
+
+// User password
+let password = " ";
+while(password !== "Admin"){
+    password = prompt("Enter Your Password")
 }
+// do while loop
