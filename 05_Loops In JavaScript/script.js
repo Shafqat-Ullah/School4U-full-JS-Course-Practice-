a8e47cf0f0d4ea3f306b5b7b13135b987215d54d
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-23 23:05:29 +0500

    update

--- 05_Loops In JavaScript/script.js
+++ 05_Loops In JavaScript/script.js
@@ -595,7 +595,39 @@
 // do while loop
 
 
-for(let i =1 ; i<5; i++){
-    console.log("Shafqat Ullah");
+// for(let i =1 ; i<5; i++){
+//     console.log("Shafqat Ullah");
+    
+// }
+
+// let i = 1;
+// while(i<10){
+//     console.log(i);
+    
+//     i++;
+
+// }
+
+
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+    
+// } while(i<5)
+
+
+// for loop
+// for(let i =1; i<10; i++){
+//     console.log(i);
+    
+// }
+
+// while loop 
+let i = 3;
+while(i<7){
+    console.log(i);
+    i++
     
 }
