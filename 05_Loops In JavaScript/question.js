08f8dfa2307fb5f4c45f02ffb2029292744c098d
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-25 11:18:45 +0500

    update

--- 05_Loops In JavaScript/question.js
+++ 05_Loops In JavaScript/question.js
@@ -9,10 +9,19 @@
 
 // question no 2
 // Calculate the sum of numbers from ‘m’ to ‘n
-let m = +(prompt(`Enter the value of m`))
-let n = +(prompt("Enter the value of n"))
-let sum= 0;
-for(let i = m; i<=n ; i++){
-    sum = sum + i;
+// let m = +(prompt(`Enter the value of m`))
+// let n = +(prompt("Enter the value of n"))
+// let sum= 0;
+// for(let i = m; i<=n ; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+let n = 50;
+for(let i = 1; i<= n; i++){
+    if(i % 2 !== 0){
+        console.log(i);
+        
+    }
 }
-console.log(sum);
