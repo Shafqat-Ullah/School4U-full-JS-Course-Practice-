5f7c488b9c86ff054582b7544d7de44ec6ef4fa9
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-25 10:47:21 +0500

    update

---
+++ 05_Loops In JavaScript/question.js
@@ -0,0 +1,10 @@
+// let num = +(prompt("Enter a number"))
+// let sum = 0;
+// for (let i = 0; i<= num; i++){
+//     sum = sum +i;
+// }
+// console.log("the sum of num is ",sum);
+
+
+
+// question no 2
