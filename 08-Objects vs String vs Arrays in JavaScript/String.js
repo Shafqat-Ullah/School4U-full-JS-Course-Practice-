e79bf7539851ed22fc64e173b8f5c4ec80b41644
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-06-27 10:23:46 +0500

    update

---
+++ 08-Objects vs String vs Arrays in JavaScript/String.js
@@ -0,0 +1,32 @@
+// String In JavaScript:
+
+// A string is a piece of text.
+// It can be a word, a sentence, a single letter, or even empty-anything made of characters.
+// In JavaScript, we write a string by putting text inside quotes:
+
+// "Schoo14U"
+// 'JavaScript'
+// "123"    
+
+// string  properties & methods 
+let a = "   Shafqat Ullah";
+let b = " khan"
+// console.log(a, a.length);
+console.log(a, a.toLocaleUpperCase());
+
+console.log(a.toLocaleLowerCase());
+
+console.log(a.trim());
+console.log(a.concat(b));
+
+console.log(a.includes("Shaf"));
+console.log(a.indexOf("q"));
+console.log(a.charAt(7));
+
+console.log(a.replace("Ullah","khan"));
+
+console.log(a.replaceAll("a","e"));
+
+// console.log(a.slice("ullah","khan"));
+console.log(a.split());
+
