7ee871387f579b0a1abf383cdcb624cdb7345070
Shafqat-Ullah <shafqatullah15305@gmail.com>
2026-07-27 23:10:31 +0500

    update

--- 10- 5 DOM PROJECT IN JS/02 List And Search/script.js
+++ 10- 5 DOM PROJECT IN JS/02 List And Search/script.js
@@ -1,38 +1,81 @@
-let userContainer = document.querySelector('.userContainer')
+// let userContainer = document.querySelector('.userContainer')
 
 
-const arr = [
-    {
-        profileUrl: '/images/Shafqat Ullah.png',
-        name :"Shafqat Ullah",
-        email : 'shafqatullah15305@gmail.com',
+// const arr = [
+//     {
+//         profileUrl: '/images/Shafqat Ullah.png',
+//         name :"Shafqat Ullah",
+//         email : 'shafqatullah15305@gmail.com',
 
-    },
+//     },
 
-     {
-        profileUrl: './images/abbas.png',
-        name :"Abbas Khan",
-        email : 'abbas123@gmail.com',
+//      {
+//         profileUrl: './images/abbas.png',
+//         name :"Abbas Khan",
+//         email : 'abbas123@gmail.com',
 
-    },
+//     },
 
-     {
-        profileUrl: 'images/ahmad.png',
-        name :"Ahmad",
-        email : 'ahmad321@gmail.com',
+//      {
+//         profileUrl: 'images/ahmad.png',
+//         name :"Ahmad",
+//         email : 'ahmad321@gmail.com',
 
-    },
+//     },
 
-     {
-        profileUrl: './images/tamoor.png',
-        name :"Tamoor Khan",
-        email : 'tamoor@gmail.com',
+//      {
+//         profileUrl: './images/tamoor.png',
+//         name :"Tamoor Khan",
+//         email : 'tamoor@gmail.com',
 
-    }
-]
+//     }
+// ]
 
-let resultArr =arr.map((object)=>{
-    let elem = document.createElement('div')
-console.log(object);
+// let resultArry = arr.map((obj)=>{
 
-})
+//     let divelem = document .createElement('div') 
+//     divelem.className='userInput'
+//     divelem.innerHTML = `
+//     <img src="./images/Shafqat Ullah.png" alt="">
+//                 <div class="userDeatils">
+//                     <h3>Shafqat Ullah</h3>
+//                 <p>shafqatullah15305@gmail.com</p>
+//                 </div>
+                
+//             </div>
+            
+
+//     `
+//    userContainer.append(divelem)
+
+           
+  
+    
+// })
+
+// // console.log(resultArry);
+
+
+// Select Elements
+const searchInput = document.querySelector("#searchInput");
+const userCards = document.querySelectorAll(".userInput");
+
+// Search Function
+searchInput.addEventListener("input", function () {
+    // Get Search Value
+    const searchValue = searchInput.value.toLowerCase().trim();
+
+    // Loop Through All User Cards
+    userCards.forEach(function (card) {
+        // Get User Name and Email
+        const name = card.querySelector("h3").textContent.toLowerCase();
+        const email = card.querySelector("p").textContent.toLowerCase();
+
+        // Check if Search Value Matches Name or Email
+        if (name.includes(searchValue) || email.includes(searchValue)) {
+            card.style.display = "flex";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
