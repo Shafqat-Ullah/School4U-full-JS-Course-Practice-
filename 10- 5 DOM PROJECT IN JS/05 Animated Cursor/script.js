544f48f31cfd2bbee85d3a2780343a2a1fd4fbb9
Shafqat Ullah <shafqatullah15305@gmail.com>
2026-08-21 00:54:05 +0500

    update

--- 10- 5 DOM PROJECT IN JS/05 Animated Cursor/script.js
+++ 10- 5 DOM PROJECT IN JS/05 Animated Cursor/script.js
@@ -1,15 +1,52 @@
+// const { current } = require("@reduxjs/toolkit");
+
 let smallCursor = document.querySelector(".smallCursor");
 let shadowCursor = document.querySelector(".shadowCursor");
-let  mouseX;
-let mouseY;
+// let  mouseX=0;
+// let mouseY=0;
 document.addEventListener ('mousemove',function(eventObj){
-    mouseX=eventObj.clientX;
-    mouseY =eventObj.clientY;
-smallCursor.style.left =mouseX  +"px"
-smallCursor.style.top =mouseY + "px"
-// shadowCursor.style.left = eventObj.clientX +"px" //es se be anmation da sakite ha
-// shadowCursor.style.top =eventObj.clientY + "px"
+    // mouseX=eventObj.clientX;
+    // mouseY =eventObj.clientY;
+smallCursor.style.left =eventObj.clientX  +"px"
+smallCursor.style.top =eventObj.clientY + "px"
+shadowCursor.style.left = eventObj.clientX +"px" //es se be anmation da sakite ha
+shadowCursor.style.top =eventObj.clientY + "px"
 }) 
-function animationShadowCursor(){
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function animationShadowCursor(){
+//     let currentX =parseFloat(shadowCursor.style.left)||0
+//     let currentY = parseFloat(shadowCursor.style.top)||0
+// let distanceX = mouseX - currentX;
+// let distanceY = mouseY -currentY;
+// shadowCursor.style.left=currentX+distanceX*0.01+"px"
+// shadowCursor.style.top=currentX+distanceY*0.01+'px'
+// requestAnimationFrame(animationShadowCursor)
+
+// }
+// animationShadowCursor();
